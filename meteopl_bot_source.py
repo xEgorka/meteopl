2736249d4dc2b4a80040d4bc4b7b6774f96c894e
foregr <you@example.com>
2018-07-17 09:13:30 +0000

    New version

--- meteopl_bot_source.py
+++ meteopl_bot_source.py
@@ -2,32 +2,60 @@
 
 from telegram.ext import Updater
 import datetime
-import pytz
+from urllib.request import Request, urlopen
+from bs4 import  BeautifulSoup
+import logging
+from telegram.ext import CommandHandler
+
 
 updater = Updater(token='TOKEN')
 dispatcher = updater.dispatcher
 
-import logging
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 def start(bot, update):
-    tz = pytz.timezone('Europe/Warsaw')
-    now = datetime.datetime.now(tz)
-    if now.hour >= 0 and now.hour < 11:
-        x = '00'
-    elif now.hour >=11 and now.hour < 17:
-        x = '06'
-    elif now.hour >=17 and now.hour < 23:
-        x = '12'
-    elif now.hour >=23 and now.hour <= 4:
-        x = '18'
-    dd = now.strftime("%Y%m%d") + x
-    url = 'https://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=' + dd + '&row=339&col=241&lang=en'
-    bot.send_photo(chat_id=update.message.chat_id, photo=url)
+    bot.send_message(chat_id=update.message.chat_id, text="To get a weather forecast send me required location or use /konigsberg shortcut to see a picture for Kaliningrad.")
 
-from telegram.ext import CommandHandler
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+
+def konigsberg(bot, update):
+    q = Request("http://m.meteo.pl/search/eu?miastoEU=&wspolrzedneEU=N54%C2%B041%27+E20%C2%B029%27&typeEU=coords&prognozaEU=60&slugEU=")
+    mybytes = urlopen(q).read()
+    mystr = mybytes.decode("utf8")
+    urlopen(q).close()
+    soup = BeautifulSoup(mystr, "html.parser")
+    url = soup.find('img', class_='border')['src']
+    url = url.replace('=pl','=en')    
+    bot.send_photo(chat_id=update.message.chat_id, photo=url)
+
+
+konigsberg_handler = CommandHandler('konigsberg', konigsberg)
+dispatcher.add_handler(konigsberg_handler)
+
+
+def location(bot, update):
+    lat = update.message.location.latitude
+    lon = update.message.location.longitude
+    lat_deg = str(int(lat))
+    lat_min = str(round((int((lat % 1)*60)),2))
+    lon_deg = str(int(lon))
+    lon_min = str(round((int((lon % 1)*60)),2))
+    q = Request("http://m.meteo.pl/search/eu?miastoEU=&wspolrzedneEU=N"+ lat_deg +"%C2%B0"+ lat_min +"%27+E"+ lon_deg +"%C2%B0"+ lon_min +"%27&typeEU=coords&prognozaEU=60&slugEU=")
+    mybytes = urlopen(q).read()
+    mystr = mybytes.decode("utf8")
+    urlopen(q).close()
+    soup = BeautifulSoup(mystr, "html.parser")
+    url = soup.find('img', class_='border')['src']
+    url = url.replace('=pl','=en')	
+    bot.send_photo(chat_id=update.message.chat_id, photo=url)
+
+from telegram.ext import MessageHandler, Filters
+location_handler = MessageHandler(Filters.location, location)
+dispatcher.add_handler(location_handler)
+	
 updater.start_polling()
