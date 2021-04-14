292e768951d36d84eca75f2e0f5da50d974c700d
Egor Maltsev <egor.maltsev@me.com>
2021-04-14 13:20:20 +0000

    New API version

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 from random import randrange
 
-updater = Updater(token='')
+#nohup python3 main.py &
+updater = Updater(token='', use_context=True)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-text = "Share some location to get a weather picture or send /Konigsberg command to see a picture for Kaliningrad.\nHow to read forecast: /legend"
+text = "Share some location to get a weather picture or send /Konigsberg command to see the picture for Kaliningrad.\nHow to read forecast: /legend"
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=text)
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=text)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-def konigsberg(bot, update):
+def konigsberg(update, context):
     q = Request("http://m.meteo.pl/row/339/col/241")
     mybytes = urlopen(q).read()
     mystr = mybytes.decode("utf8")
@@ -29,14 +30,14 @@ def konigsberg(bot, update):
     soup = BeautifulSoup(mystr, "html.parser")
     url = soup.find('img', class_='border')['src']
     url = url.replace('=pl','=en')
-    bot.send_photo(chat_id=update.message.chat_id, photo=url)
+    context.bot.send_photo(chat_id=update.message.chat_id, photo=url)
     if randrange(10) == 0:
-        bot.send_message(chat_id=update.message.chat_id, text="Try to share any location to get forecast for that place! 📍")
+        context.bot.send_message(chat_id=update.message.chat_id, text="Try to share any location to get forecast for that place! 📍")
 
 konigsberg_handler = CommandHandler('konigsberg', konigsberg)
 dispatcher.add_handler(konigsberg_handler)
 
-def location(bot, update):
+def location(update, context):
     lat = update.message.location.latitude
     lon = update.message.location.longitude
     url = "http://www.meteo.pl/um/php/mgram_search.php?NALL=" + str(lat) + "&EALL=" + str(lon) + "&lang=en"
@@ -56,15 +57,15 @@ def location(bot, update):
     soup = BeautifulSoup(mystr, "html.parser")
     url = soup.find('img', class_='border')['src']
     url = url.replace('=pl','=en')
-    bot.send_photo(chat_id=update.message.chat_id, photo=url)
+    context.bot.send_photo(chat_id=update.message.chat_id, photo=url)
 
 from telegram.ext import MessageHandler, Filters
 location_handler = MessageHandler(Filters.location, location)
 dispatcher.add_handler(location_handler)
 
-def legend(bot, update):
+def legend(update, context):
     url = "https://www.meteo.pl/um/metco/leg_um_en_cbase_256.png"
-    bot.send_photo(chat_id=update.message.chat_id, photo=url)
+    context.bot.send_photo(chat_id=update.message.chat_id, photo=url)
 
 legend_handler = CommandHandler('legend', legend)
 dispatcher.add_handler(legend_handler)
