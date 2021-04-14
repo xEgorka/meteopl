27d12064d3caae01bcd25734bf152f5fed94e46d
Egor Maltsev <egor.maltsev@me.com>
2021-04-14 11:06:55 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # meteopl_bot
+Simple meteo Telegram bot allows you to get weather forecast for Europe from http://meteo.pl
--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 #!/usr/bin python3
-from telegram.ext import Updater
 import datetime
-from urllib.request import Request, urlopen
-from bs4 import  BeautifulSoup
 import logging
+
+from telegram.ext import Updater
 from telegram.ext import CommandHandler
+
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 from random import randrange
 
 updater = Updater(token='')
 dispatcher = updater.dispatcher
-
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+text = "Share some location to get a weather picture or send /Konigsberg command to see a picture for Kaliningrad.\nHow to read forecast: /legend"
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Share some location to get a weather picture or send /Konigsberg command to see a picture for Kaliningrad.\nHow to read forecast: /legend")
-
+    bot.send_message(chat_id=update.message.chat_id, text=text)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-
 def konigsberg(bot, update):
-    '''q = Request("http://m.meteo.pl/search/eu?miastoEU=&wspolrzedneEU=N54%C2%B041%27+E20%C2%B029%27&typeEU=coords&prognozaEU=60&slugEU=")'''
     q = Request("http://m.meteo.pl/row/339/col/241")
     mybytes = urlopen(q).read()
     mystr = mybytes.decode("utf8")
@@ -35,10 +33,8 @@ def konigsberg(bot, update):
     if randrange(10) == 0:
         bot.send_message(chat_id=update.message.chat_id, text="Try to share any location to get forecast for that place! 📍")
 
-
 konigsberg_handler = CommandHandler('konigsberg', konigsberg)
 dispatcher.add_handler(konigsberg_handler)
-
 
 def location(bot, update):
     lat = update.message.location.latitude
@@ -53,7 +49,6 @@ def location(bot, update):
     lat_min = str(round((int((lat % 1)*60)),2))
     lon_deg = str(int(lon))
     lon_min = str(round((int((lon % 1)*60)),2))
-    #q = Request("http://m.meteo.pl/search/eu?miastoEU=&wspolrzedneEU=N"+ lat_deg +"%C2%B0"+ lat_min +"%27+E"+ lon_deg +"%C2%B0"+ lon_min +"%27&typeEU=coords&prognozaEU=60&slugEU=")
     q = Request("http://m.meteo.pl/row/" + str(row) + "/col/" + str(col))
     mybytes = urlopen(q).read()
     mystr = mybytes.decode("utf8")
@@ -66,7 +61,6 @@ def location(bot, update):
 from telegram.ext import MessageHandler, Filters
 location_handler = MessageHandler(Filters.location, location)
 dispatcher.add_handler(location_handler)
-
 
 def legend(bot, update):
     url = "https://www.meteo.pl/um/metco/leg_um_en_cbase_256.png"
